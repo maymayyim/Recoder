ec7aa326ce01576bbfd9f67691bd41a122fc3db5
maymayyim <mayyim.putti@gmail.com>
2017-11-02 12:05:55 +0700

    First Commit

--- Recoder/ViewController.swift
+++ Recoder/ViewController.swift
@@ -7,19 +7,64 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, AVAudioPlayerDelegate {
+    var audioPlayer: AVAudioPlayer?
+    var audioRecord: AVAudioRecorder?
+
+    @IBOutlet weak var playBtn: UIButton!
+    @IBOutlet weak var recordBtn: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let fileManager = FileManager()
+        let documentsFolderUrl = try? fileManager.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
+        
+        let fileURL = documentsFolderUrl?.appendingPathComponent("Recording.m4a")
+        
+        let audioSetting = [AVFormatIDKey : kAudioFormatMPEG4AAC as NSNumber, AVSampleRateKey : 16000.0 as NSNumber, AVNumberOfChannelsKey : 1 as NSNumber, AVEncoderAudioQualityKey : AVAudioQuality.low.rawValue as NSNumber]
+        
+        
+        var error : NSError?
+        do {
+            audioRecord = try AVAudioRecorder(url: fileURL!, settings: audioSetting)
+        } catch var error1 as NSError {
+            error = error1
+            audioRecord = nil
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    @IBAction func recordMethod(_ sender: Any) {
+        if(audioRecord!.isRecording){
+            audioRecord!.stop()
+            recordBtn.setTitle("Record", for: UIControlState.normal)
+            playBtn.isEnabled = true
+        }else{
+            audioRecord!.record()
+            recordBtn.setTitle("Stop", for: UIControlState.normal)
+            playBtn.isEnabled = false
+        }
+        
+    }
+    @IBAction func playMethod(_ sender: Any) {
+        if(!(audioRecord?.isRecording)!){
+            do{
+                audioPlayer = try AVAudioPlayer(contentsOf: (audioRecord?.url)!)
+            }catch let error as NSError{
+                audioPlayer = nil
+            }
+            audioPlayer?.play()
+        }
+        
+    }
+    
 
 }
 
